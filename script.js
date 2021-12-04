758326505ec0910f2aa9103c68d6244d19a072eb
khushijain3103 <khushijain312003@gmail.com>
2021-12-04 23:56:13 +0530

    js edited

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 async function gimmeData() {
-    const response= await fetch('https://cors-anywhere.herokuapp.com/http://api.exchangeratesapi.io/v1/latest?access_key=3d02f46508b9b4c0a08f1aa0b66cc9d1&format=1',
-    {
-        "headers":{
-            
-        },
-        "mode":'cors'
-    });
+    const response= await fetch('http://api.exchangeratesapi.io/v1/latest?access_key=3d02f46508b9b4c0a08f1aa0b66cc9d1&format=1');
+    // {
+    //     "headers":{
+
+    //     },
+    //     "mode":'cors'
+    // });
     const data= await response.json();
 
     return data;
 }
 async function symbols(){
-    const sym = await fetch('https://cors-anywhere.herokuapp.com/http://api.exchangeratesapi.io/v1/latest?access_key=3d02f46508b9b4c0a08f1aa0b66cc9d1&format=1');
+    const sym = await fetch('http://api.exchangeratesapi.io/v1/latest?access_key=3d02f46508b9b4c0a08f1aa0b66cc9d1&format=1');
     const symData = await sym.json();
 
     return symData;
 }
 
-const mysyms = symbols();
-console.log(mysyms);
 const printData = gimmeData();
 console.log(printData);
+const mysyms = symbols();
+console.log(mysyms);
