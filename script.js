579ca8046081e3bc470b876d8e57f79e625355a9
khushijain3103 <khushijain312003@gmail.com>
2021-12-05 00:10:54 +0530

    js edited

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 async function gimmeData() {
-    const response= await fetch('http://api.exchangeratesapi.io/v1/latest?access_key=3d02f46508b9b4c0a08f1aa0b66cc9d1&format=1');
-    // {
-    //     "headers":{
-
-    //     },
-    //     "mode":'cors'
-    // });
+    const response= await fetch("https://cors-anywhere.herokuapp.com/http://api.exchangeratesapi.io/v1/latest?access_key=3d02f46508b9b4c0a08f1aa0b66cc9d1&format=1", {
+        headers : {
+            "Accept": "application/json",
+            "Origin": "github.com",
+            "x-requested-with":"XMLHttpRequest"
+        },
+        mode: "cors"
+    })
     const data= await response.json();
 
     return data;
 }
 async function symbols(){
-    const sym = await fetch('http://api.exchangeratesapi.io/v1/latest?access_key=3d02f46508b9b4c0a08f1aa0b66cc9d1&format=1');
+    const sym = await fetch("https://cors-anywhere.herokuapp.com/http://api.exchangeratesapi.io/v1/latest?access_key=3d02f46508b9b4c0a08f1aa0b66cc9d1&format=1", {
+        headers : {
+            "Accept": "application/json",
+            "Origin": "github.com",
+            "x-requested-with":"XMLHttpRequest"
+        },
+        mode: "cors"
+    })
     const symData = await sym.json();
 
     return symData;
