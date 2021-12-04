101e5ae87d117a1ffa349a7812bc141ce34285b8
khushijain3103 <khushijain312003@gmail.com>
2021-12-04 22:34:28 +0530

    hdgiwe

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function gimmeData() {
-    const response= await fetch('http://api.exchangeratesapi.io/v1/latest?access_key=3d02f46508b9b4c0a08f1aa0b66cc9d1');
+    const response= await fetch('https://cors-anywhere.herokuapp.com/http://api.exchangeratesapi.io/v1/latest?access_key=3d02f46508b9b4c0a08f1aa0b66cc9d1&format=1');
     const data= await response.json();
 
     return data;
